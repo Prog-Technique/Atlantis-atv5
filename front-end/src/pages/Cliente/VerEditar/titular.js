7e2c14c982e40b280c4dd850fef33c8b30991c2f
amandavo <amandavoliveira0@gmail.com>
2023-06-15 08:56:09 -0300

    resolucao do ver/editar

--- front-end/src/pages/Cliente/VerEditar/titular.js
+++ front-end/src/pages/Cliente/VerEditar/titular.js
@@ -157,128 +157,115 @@ export default function VerCliente() {
           <div className="content">
 
             <div className="input">
-              <label>Nome Completo:</label>
-              <input placeholder='Insira o nome completo' type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
+              <label>Nome</label>
+              <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
             </div>
 
-            <div className="campo-duplo">
-              <div className="field esquerda">
-                <label>Nome social:</label>
-                <input placeholder='Insira o nome social' type="text" value={nome_social} onChange={(e) => setNomeSocial(e.target.value)} required />
-              </div>
-              <div className="field direita">
-                <label>Nascimento:</label>
-                <input type="date" value={data_nasc} onChange={(e) => setData_nasc(e.target.value)} required />
-              </div>
+            <div className="input">
+              <label>Nome social</label>
+              <input type="text" value={nome_social} onChange={(e) => setNomeSocial(e.target.value)} required />
+            </div>
+            <div className="input">
+              <label>Nascimento</label>
+              <input type="date" value={data_nasc} onChange={(e) => setData_nasc(e.target.value)} required />
             </div>
 
             {telefones.map((telefone, index) => (
-              <div className="field" key={index}>
-                <label>Telefone:</label>
-                <input
-                  placeholder='XX XXXXXXXX'
-                  type="text"
-                  value={telefone.numero}
+              <div className="input" key={index}>
+                <label>Telefone</label>
+                <input type="text" value={telefone.numero} required
                   onChange={(e) => {
                     const newTelefones = [...telefones];
                     newTelefones[index].numero = e.target.value;
                     setTelefones(newTelefones);
-                  }}
-                  required
-                />
+                  }} />
               </div>
             ))}
 
             <FiPlusCircle size={25} className="plusCircle"
               onClick={() => addFormTelefone()} />
+            </div>
 
-            <div className="campo-duplo">
-              <div className="field esquerda">
-                <label>CPF:</label>
-                <input type="text" placeholder='XXX.XXX.XXX-XX' value={cpf} onChange={(e) => setCpf(e.target.value)} required />
-              </div>
-              <div className="field direita">
-                <label>Passaporte:</label>
-                <input type="text" placeholder='XXX.XXX.XXX-XX' value={passaporte} onChange={(e) => setPassaporte(e.target.value)} required />
-              </div>
+            <div className="content">
+            <div className="input">
+              <label>CPF</label>
+              <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} required />
+            </div>
+            <div className="input">
+              <label>Passaporte</label>
+              <input type="text" value={passaporte} onChange={(e) => setPassaporte(e.target.value)} required />
             </div>
 
             {rgs.map((rg, index) => (
-              <div className="campo-duplo" key={index}>
-                <div className="field esquerda">
-                  <label>RG:</label>
-                  <input
-                    type="text"
-                    placeholder='XX.XXX.XXX-X'
-                    value={rg.numero}
+              <div key={index}>
+                <div className="input">
+                  <label>RG</label>
+                  <input type="text" value={rg.numero} required
                     onChange={(e) => {
                       const newRgs = [...rgs];
                       newRgs[index].numero = e.target.value;
                       setRgs(newRgs);
-                    }}
-                    required
-                  />
+                    }} />
                 </div>
-                <div className="field direita">
-                  <label>RG data de emissão:</label>
-                  <input
-                    type="date"
-                    value={rg.emissao}
+
+                <div className="input">
+                  <label>Data de emissão</label>
+                  <input type="date" value={rg.emissao} required
                     onChange={(e) => {
                       const newRgs = [...rgs];
                       newRgs[index].emissao = e.target.value;
                       setRgs(newRgs);
-                    }}
-                    required
-                  />
+                    }} />
                 </div>
               </div>
             ))}
 
             <FiPlusCircle size={25} className="plusCircle"
               onClick={() => addFormRg()} />
-
-            <div className="field ">
-              <label>CEP:</label>
-              <input type="text" placeholder='XXXXX-XXX' value={cep} onChange={(e) => setCep(e.target.value)} onBlur={buscarEndereco} required />
+            
             </div>
 
-            <div className="campo-duplo">
-              <div className="field esquerda">
-                <label>Locadouro:</label>
-                <input placeholder='Insira o nome do locadouro' type="text" value={rua} onChange={(e) => setRua(e.target.value)} required />
-              </div>
-              <div className="field direita">
-                <label>Número:</label>
-                <input placeholder='Número' type="text" value={numero} onChange={(e) => setNumero(e.target.value)} required />
-              </div>
+            <div className="content">
+            <div className="input ">
+              <label>CEP</label>
+              <input type="text" value={cep} onChange={(e) => setCep(e.target.value)} onBlur={buscarEndereco} required />
             </div>
 
-            <div className="field">
-              <label>Bairro:</label>
-              <input placeholder='Insira o nome do bairro' type="text" value={bairro} onChange={(e) => setBairro(e.target.value)} required />
+            <div className="input">
+              <label>Número</label>
+              <input type="text" value={numero} onChange={(e) => setNumero(e.target.value)} required />
             </div>
 
-            <div className="field">
-              <label>Cidade:</label>
-              <input placeholder='Insira o nome da cidade' type="text" value={cidade} onChange={(e) => setCidade(e.target.value)} required />
+            <div className="input">
+              <label>Logradouro</label>
+              <input type="text" value={rua} onChange={(e) => setRua(e.target.value)} required />
             </div>
 
-            <div className="campo-duplo">
-              <div className="field esquerda">
-                <label>Estado:</label>
-                <input placeholder='Insira o nome do estado' type="text" value={estado} onChange={(e) => setEstado(e.target.value)} required />
-              </div>
-              <div className="field direita">
-                <label>País:</label>
-                <input placeholder='Insira o nome do país' type="text" value={pais} onChange={(e) => setPais(e.target.value)} required />
-              </div>
+            <div className="input">
+              <label>Bairro</label>
+              <input type="text" value={bairro} onChange={(e) => setBairro(e.target.value)} required />
+            </div>
+            </div>
+
+            <div className="content">
+            <div className="input">
+              <label>Cidade</label>
+              <input type="text" value={cidade} onChange={(e) => setCidade(e.target.value)} required />
+            </div>
+
+            <div className="input">
+              <label>Estado</label>
+              <input type="text" value={estado} onChange={(e) => setEstado(e.target.value)} required />
+            </div>
+            <div className="input">
+              <label>País</label>
+              <input type="text" value={pais} onChange={(e) => setPais(e.target.value)} required />
             </div>
 
             <button type='button' onClick={() => addDependentes()}>Ver/Editar dependente(s)</button>
             <button type='submit'>SALVAR</button>
+            </div>
 
-          </div>
         </form>
       </div>
     </>
